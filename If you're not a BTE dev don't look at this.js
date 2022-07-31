b1debd3faf74c65d8c4b6555876e0c03cbe6eb5c
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-07-31 10:05:57 -0700

    Update If you're not a BTE dev don't look at this.js

--- If you're not a BTE dev don't look at this.js
+++ If you're not a BTE dev don't look at this.js
@@ -1,1 +1,1 @@
-javascript: (() => {var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/gh/NVB9ALT/Messerchmitt-Me262-code@Addon-files/3d buildings core JS.js"; document.body.appendChild(script);script.onload = function (){runBuildings()}})();
+javascript: (() => {var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/gh/NVB9ALT/Messerchmitt-Me262-code@Addon-files/3d buildings core JS E.js"; document.body.appendChild(script);script.onload = function (){runBuildings()}})();
